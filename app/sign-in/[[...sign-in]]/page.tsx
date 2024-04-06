43a05ced965b1942e6688a9d46cb6f8354528958
Amirhossein Zandi <blackviper933@gmail.com>
2024-04-06 14:59:10 +0330

    "Update sign-in page to redirect to /about after sign-in; adjust middleware public routes"
    This commit updates the sign-in page to redirect to the /about page after a successful sign-in. The middleware public routes have also been adjusted to only include the root route ('/').

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,12 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
+
+  
   return (
     <>
       <p className="text-center">this is sign-in page</p>
-      <SignIn />
+      <SignIn afterSignInUrl={"/about"}/>
     </>
   )
 }
